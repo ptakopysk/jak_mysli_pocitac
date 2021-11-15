7071be94c5fb6231c445dd813f75527d01d84303
Rudolf Rosa <rosa@ufal.mff.cuni.cz>
2021-11-15 01:13:49 +0100

    lineární normované obrázky

--- embedinky_jako_ctverecky.py
+++ embedinky_jako_ctverecky.py
@@ -9,6 +9,8 @@ import matplotlib
 import matplotlib.pyplot as plt
 import regex as re
 
+import argparse
+
 import logging
 logging.basicConfig(
     format='%(asctime)s %(message)s',
@@ -16,21 +18,21 @@ logging.basicConfig(
     # level=logging.DEBUG)
     level=logging.INFO)
 
+ap = argparse.ArgumentParser()
+ap.add_argument('--FILE', help='embeddings file', default='cc.cs.300.vec.gz')
+ap.add_argument('--LIMIT', help='read this many embeddings', default=100000)
+ap.add_argument('--MAXLINES', help='read this many text lines', default=10)
+ap.add_argument('--TXTFILE', help='read text from this file', default="olasce.txt")
+ap.add_argument('--N', help='side of square (so a square fits N^2 dimensions)', default=17)
 
-FILE='cc.cs.300.vec.gz'
-LIMIT=100000
+args = ap.parse_args()
 
-N = 17
-
-TXTFILE="genesis.txt"
-
-# [-1, 0, 0, ..., 0, 0, 1]
-EMPTY_EMB = [-1] + [0 for _ in range(N**2-2)] + [1]
+EMPTY_EMB = [0 for _ in range(args.N**2)]
 
 embeddings = dict()
 
-logging.info(f'Načítám embedinky ze souboru {FILE}')
-with gzip.open(FILE, 'rt') as infile:
+logging.info(f'Načítám embedinky ze souboru {args.FILE}')
+with gzip.open(args.FILE, 'rt') as infile:
     # header
     header = infile.readline().split()
     logging.info(f'Soubor obsahuje embedinky dimenze {header[1]} pro {header[0]} slov')
@@ -39,28 +41,34 @@ with gzip.open(FILE, 'rt') as infile:
         word = fields[0]
         emb = [float(x) for x in fields[1:]]
         embeddings[word] = emb
-        if len(embeddings) >= LIMIT:
+        if len(embeddings) >= args.LIMIT:
             break
-logging.info(f'Načetl jsem {len(embeddings)} embedinků ze souboru {FILE}')
+logging.info(f'Načetl jsem {len(embeddings)} embedinků ze souboru {args.FILE}')
+
+def avg(list):
+    return sum(list)/len(list)
+
+def absavg(list):
+    return sum([abs(x) for x in list])/len(list)
 
 def draw_word(word, emb, ax):    
     # push embeddings into a square matrix
-    data = np.reshape(emb[:N**2], (N, N))
+    data = np.reshape(emb[:args.N**2], (args.N, args.N))
     # text label of x axis
     ax.set_xlabel(word)
     # from blue to red
-    ax.imshow(data, cmap='seismic')
+    # ax.imshow(data, cmap='seismic', vmin=-1, vmax=1)
+    ax.imshow(data, cmap='seismic', vmin=-0.5, vmax=0.5)
 
-logging.info(f'Načítám text ze souboru {TXTFILE}')
-with open(TXTFILE) as intext:
+logging.info(f'Načítám text ze souboru {args.TXTFILE}')
+with open(args.TXTFILE) as intext:
     text = list()
     for line in intext:
         words = line.split()
         text.append(words)
-    logging.info(f'Načetl jsem {len(text)} řádek ze souboru {TXTFILE}')
+    logging.info(f'Načetl jsem {len(text)} řádek ze souboru {args.TXTFILE}')
 
-# !!!!
-text = text[:10]
+text = text[:args.MAXLINES]
 
 rows = len(text)
 cols = max([len(line) for line in text])
@@ -85,9 +93,12 @@ for line, row in zip(text, axs):
     for word, ax in zip(line, row):
         cleaned_word = re.sub(r'[^[:alpha:]]', '', word)
         if cleaned_word in embeddings:
-            draw_word(word, embeddings[cleaned_word][:N**2], ax)
+            emb = embeddings[cleaned_word][:args.N**2]
+            draw_word(cleaned_word, emb, ax)
+            #print(word, 'MAX:', max(emb), 'MIN:', min(emb),
+            #        'AVG:', avg(emb), 'ABSAVG:', absavg(emb))
         else:
-            draw_word(word, EMPTY_EMB, ax)
+            draw_word(cleaned_word, EMPTY_EMB, ax)
 
 logging.info(f'Vykresluji obrázek')
 
