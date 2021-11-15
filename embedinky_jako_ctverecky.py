52ac428014342a208974561a5f12cde40233bee3
Rudolf Rosa <rosa@ufal.mff.cuni.cz>
2021-11-15 02:19:35 +0100

    reprezentace pomocí čárových diagramů

--- embedinky_jako_ctverecky.py
+++ embedinky_jako_ctverecky.py
@@ -25,17 +25,19 @@ ap.add_argument('--MAXLINES', help='read this many text lines', default=10)
 ap.add_argument('--TXTFILE', help='read text from this file', default="kava.txt")
 ap.add_argument('--N', help='side of square (so a square fits N^2 dimensions)', default=17)
 ap.add_argument('--SORT', help='show dimensions sorted', default=False)
+ap.add_argument('--DRAWLINES', help='represent by lines', default=False)
 
 args = ap.parse_args()
 
-EMPTY_EMB = [0 for _ in range(args.N**2)]
-
 embeddings = dict()
+
+embeddings_dim = 0
 
 logging.info(f'Načítám embedinky ze souboru {args.FILE}')
 with gzip.open(args.FILE, 'rt') as infile:
     # header
     header = infile.readline().split()
+    embeddings_dim = int(header[1])
     logging.info(f'Soubor obsahuje embedinky dimenze {header[1]} pro {header[0]} slov')
     for line in infile:
         fields = line.split()
@@ -46,23 +48,55 @@ with gzip.open(args.FILE, 'rt') as infile:
             break
 logging.info(f'Načetl jsem {len(embeddings)} embedinků ze souboru {args.FILE}')
 
+EMPTY_EMB = [0 for _ in range(embeddings_dim)]
+
+import math
+if args.DRAWLINES:
+    drawlines_rows = int((embeddings_dim/6)**0.5)
+    drawlines_cols = int(math.ceil(embeddings_dim/6/drawlines_rows))
+    logging.info(f'Budu kreslit čárovou reprezentaci o {drawlines_rows} řádcích a {drawlines_cols} sloupcích')
+
+    drawlines = list()
+    for row in range(drawlines_rows):
+        for col in range(drawlines_cols):
+            x = 2*col
+            y = -2*row
+            # svislý čáry
+            drawlines.append( ([x, x], [y, y-2]) )
+            drawlines.append( ([x+1, x+1], [y, y-2]) )
+            # vodorovný čáry
+            drawlines.append( ([x, x+2], [y, y]) )
+            drawlines.append( ([x, x+2], [y-1, y-1]) )
+            # čáry křížem
+            drawlines.append( ([x, x+2], [y, y-2]) )
+            drawlines.append( ([x, x+2], [y-2, y]) )
+
 def avg(list):
     return sum(list)/len(list)
 
 def absavg(list):
     return sum([abs(x) for x in list])/len(list)
 
+def absmax1(number):
+    a = abs(number)
+    return a if a < 1 else 1
+
 def draw_word(word, emb, ax):    
-    # push embeddings into a square matrix
-    emb = emb[:args.N**2]
-    if args.SORT:
-        emb = sorted(emb)
-    data = np.reshape(emb, (args.N, args.N))
     # text label of x axis
     ax.set_xlabel(word)
-    # from blue to red
-    # ax.imshow(data, cmap='seismic', vmin=-1, vmax=1)
-    ax.imshow(data, cmap='seismic', vmin=-0.5, vmax=0.5)
+    if args.DRAWLINES:
+        for dim, (xs, ys) in zip (emb, drawlines):
+            color = 'b' if dim < 0 else 'r'
+            ax.plot(xs, ys, color, alpha=absmax1(dim))
+    else:
+        # push embeddings into a square matrix
+        emb = emb[:args.N**2]
+        if args.SORT:
+            emb = sorted(emb)
+        data = np.reshape(emb, (args.N, args.N))
+        # from blue to red
+        # ax.imshow(data, cmap='seismic', vmin=-1, vmax=1)
+        ax.imshow(data, cmap='seismic', vmin=-0.5, vmax=0.5)
 
 logging.info(f'Načítám text ze souboru {args.TXTFILE}')
 with open(args.TXTFILE) as intext:
@@ -97,7 +131,7 @@ for line, row in zip(text, axs):
     for word, ax in zip(line, row):
         cleaned_word = re.sub(r'[^[:alpha:]]', '', word)
         if cleaned_word in embeddings:
-            emb = embeddings[cleaned_word][:args.N**2]
+            emb = embeddings[cleaned_word]
             draw_word(cleaned_word, emb, ax)
             #print(word, 'MAX:', max(emb), 'MIN:', min(emb),
             #        'AVG:', avg(emb), 'ABSAVG:', absavg(emb))
